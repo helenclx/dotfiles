8bbe2fd3cf10a1b9bdc7bb5b260e2ced524a61ec
Helen Chong <contact@helenchong.dev>
2025-06-29 22:36:03 +0800

    Add step to change Bash config to my own

--- setup-home.sh
+++ setup-home.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 
+# Set Bash configuration to the one in my yadm repo
+yadm restore .bashrc
+
 # Install software and packages for my personal use
-sudo dnf install -y zsh zoxide vim fcitx5 fcitx5-chinese-addons neovim
+sudo dnf install -y zsh zoxide vim fcitx5 fcitx5-chinese-addons easyeffects neovim
