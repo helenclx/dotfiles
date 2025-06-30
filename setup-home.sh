e83c46bd7a597a9c8ee97001168f11a57458eeb4
Helen Chong <contact@helenchong.dev>
2025-06-30 10:04:47 +0800

    Remove Neovim from script to set up home
    Set up Neovim manually

--- setup-home.sh
+++ setup-home.sh
@@ -3,5 +3,5 @@
 # Set Bash configuration to the one in my yadm repo
 yadm restore .bashrc
 
-# Install software and packages for my personal use
-sudo dnf install -y zsh zoxide vim fcitx5 fcitx5-chinese-addons easyeffects neovim
+# Install software and packages that use my custom configs
+sudo dnf install -y zsh zoxide vim fcitx5 fcitx5-chinese-addons easyeffects
