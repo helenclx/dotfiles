4ea15429f3d41e8d435b7fd4009563496853b82d
Helen Chong <contact@helenchong.dev>
2025-07-06 15:16:34 +0800

    chore(setup): add ghostwriter to install command

--- setup-home.sh
+++ setup-home.sh
@@ -8,4 +8,4 @@ echo "Change Bash configuration to the one in my dot file repo..."
 yadm restore .bashrc
 
 echo "Install additional packages that use these dot files..."
-sudo dnf install -y zsh zoxide vim fcitx5 fcitx5-chinese-addons easyeffects
+sudo dnf install -y zsh zoxide vim fcitx5 fcitx5-chinese-addons easyeffects ghostwriter
