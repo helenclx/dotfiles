77163aa41bdb29c577aa06d32accdd5422aaebf8
Helen Chong <contact@helenchong.dev>
2025-09-03 09:44:17 +0800

    chore: add bash and fish shell alias for kitten icat

--- .bashrc.d/aliases.sh
+++ .bashrc.d/aliases.sh
@@ -1,4 +1,5 @@
 alias bathelp='bat --plain --language=help'
 alias eza='eza --icons=auto'
 alias grep='grep --color=auto'
+alias icat='kitten icat'
 alias ls='ls --color=auto'
--- .config/fish/conf.d/aliases.fish
+++ .config/fish/conf.d/aliases.fish
@@ -1,2 +1,3 @@
 alias bathelp "bat --plain --language=help"
 alias eza "eza --icons auto"
+alias icat='kitten icat'
