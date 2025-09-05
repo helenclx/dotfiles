606fe42b209458448ba42f653ae95fda7595b38a
Helen Chong <contact@helenchong.dev>
2025-09-05 11:59:16 +0800

    chore: add syntax highlighting to less via shell environment variables

--- .config/fish/conf.d/environment.fish
+++ .config/fish/conf.d/environment.fish
@@ -1,4 +1,9 @@
 # Set default editor
 export EDITOR=nvim
+
+# syntax highlighting for the less program
+export LESSOPEN="| /usr/bin/src-hilite-lesspipe.sh %s"
+export LESS=' -R'
+
 # Set ripgrep config location
 export RIPGREP_CONFIG_PATH="$HOME/.config/ripgrep/ripgreprc"
