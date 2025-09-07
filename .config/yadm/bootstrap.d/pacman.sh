dc45d7103f5f5f79ae0ca0e5dbad724ce1bcc016
Helen Chong <contact@helenchong.dev>
2025-09-08 00:21:37 +0800

    chore(bootstrap): remove variables for pacman installation

--- .config/yadm/bootstrap.d/pacman.sh
+++ .config/yadm/bootstrap.d/pacman.sh
@@ -1,11 +1,7 @@
 #!/usr/bin/env bash
 
-software_deps="calf curl chafa fd jq lsp-plugins-lv2 noto-fonts-cjk python python-pipx unzip"
-
-pacman_packages="bash bat easyeffects elisa eza fcitx5 fcitx5-chinese-addons fcitx5-qt fcitx5-gtk fcitx5-configtool fish foliate fzf ghostty ghostwriter kate kcharselect kitty konsole lazygit mpv mpv-mpris neovim ripgrep starship tmux vim zoxide"
-
 echo "Installing software dependencies from Arch Linux's repository..."
-sudo pacman -S --needed "$software_deps"
+sudo pacman -S --needed calf curl chafa fd jq lsp-plugins-lv2 noto-fonts-cjk python python-pipx unzip
 
 echo "Installing main software from Arch Linux's repository..."
-sudo pacman -S --needed "$pacman_packages"
+sudo pacman -S --needed bash bat easyeffects elisa eza fcitx5 fcitx5-chinese-addons fcitx5-qt fcitx5-gtk fcitx5-configtool fish foliate fzf ghostty ghostwriter kate kcharselect kitty konsole lazygit mpv mpv-mpris neovim ripgrep starship tmux vim zoxide
