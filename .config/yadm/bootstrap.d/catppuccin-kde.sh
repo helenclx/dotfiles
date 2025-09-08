428eaf64fbc1164a6c55cbd62a4c734b79d2feb4
Helen Chong <contact@helenchong.dev>
2025-09-08 18:30:30 +0800

    chore(bootstrap): ensure navigation to the home directory's Downloads folder before installing Catppuccin global KDE theme

--- .config/yadm/bootstrap.d/catppuccin-kde.sh
+++ .config/yadm/bootstrap.d/catppuccin-kde.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Cloning the Catppuccin global theme for KDE Plasma into the Downloads folder..."
-mkdir -p Downloads && cd Downloads || return
+mkdir -p "$HOME/Downloads" && cd "$HOME/Downloads" || return
 git clone --depth=1 https://github.com/catppuccin/kde catppuccin-kde && cd catppuccin-kde || return
 echo "Installing the Catppuccin global theme for KDE Plasma..."
 chmod +x install.sh
