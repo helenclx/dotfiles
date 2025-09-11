8d471060bd9ab082cd0ebb2dfce37954a5f0fa7b
Helen Chong <contact@helenchong.dev>
2025-09-12 01:06:14 +0800

    fix(bootstrap): remove redendant commands from the Catppuccin KDE global theme script

--- .config/yadm/bootstrap.d/catppuccin-kde.sh
+++ .config/yadm/bootstrap.d/catppuccin-kde.sh
@@ -6,9 +6,4 @@ git clone --depth=1 https://github.com/catppuccin/kde catppuccin-kde && cd catpp
 echo "Installing the Catppuccin global theme for KDE Plasma..."
 chmod +x install.sh
 ./install.sh
-cd "$HOME" || returnmkdir -p Downloads && cd Downloads || return
-git clone --depth=1 https://github.com/catppuccin/kde catppuccin-kde && cd catppuccin-kde || return
-echo "Installing the Catppuccin global theme for KDE Plasma..."
-chmod +x install.sh
-./install.sh
 cd "$HOME" || return
