017656285dab1a045e3294635e0064b963b7dd24
Helen Chong <contact@helenchong.dev>
2025-09-12 13:16:18 +0800

    chore: remove Bun from the PATH environment variable in bash and fish shells

--- .bashrc.d/path.sh
+++ .bashrc.d/path.sh
@@ -2,7 +2,3 @@
 export PATH="$PATH:$HOME/Executables"
 export PATH="$PATH:$HOME/bin"
 export PATH="$PATH:$HOME/.local/bin"
-
-# Bun
-export BUN_INSTALL="$HOME/.bun"
-export PATH="$BUN_INSTALL/bin:$PATH"
--- .config/fish/conf.d/path.fish
+++ .config/fish/conf.d/path.fish
@@ -3,7 +3,3 @@ fish_add_path -P $HOME/Executables
 fish_add_path -P $HOME/bin
 fish_add_path -P $HOME/.local/bin
 fish_add_path -P $HOME/.local/share/gem/ruby/3.4.0/bin
-
-# Bun
-export BUN_INSTALL="$HOME/.bun"
-export PATH="$BUN_INSTALL/bin:$PATH"
