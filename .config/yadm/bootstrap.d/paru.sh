57e1d2a673d7f48080cb3a9f4f3a73f509fc9a56
Helen Chong <contact@helenchong.dev>
2025-10-04 14:13:30 +0800

    chore: switch to paru as my AUR helper of choice

--- .config/yadm/bootstrap.d/paru.sh
+++ .config/yadm/bootstrap.d/paru.sh
@@ -2,7 +2,7 @@
 
 sudo pacman -S --needed git base-devel
 mkdir -p Downloads && cd Downloads || return
-git clone https://aur.archlinux.org/yay.git
-cd yay
+git clone https://aur.archlinux.org/paru.git
+cd paru
 makepkg -si
 cd "$HOME" || return
