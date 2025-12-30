1d34946fdbda532c3af9b8cbaefbdf5dfbab1eba
Helen Chong <contact@helenchong.dev>
2025-12-31 07:55:00 +0800

    chore(fish): set shell theme in fish's config file instead of bootstrap script

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,9 @@
 # Disable fish greeting
 set -g fish_greeting
 
+# Set theme for fish shell
+fish_config theme choose "Catppuccin Mocha"
+
 # Integrate certain programs into the shell
 fzf --fish | source
 # The following lines must be added to the end of this config file
