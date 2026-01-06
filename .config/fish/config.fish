3f5623ab8a594d4dabf6af80cc12fe897988db10
Helen Chong <contact@helenchong.dev>
2026-01-06 21:28:16 +0800

    chore(fish): remove --color-theme=dark argument from the theme setting configuration

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@
 set -g fish_greeting
 
 # Set theme for fish shell
-fish_config theme choose "Catppuccin Mocha" --color-theme=dark
+fish_config theme choose "Catppuccin Mocha"
 
 # Integrate certain programs into the shell
 fzf --fish | source
