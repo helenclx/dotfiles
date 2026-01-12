04e3a8038b2a56aa4a8bb1af44cdb2a71a7d540d
Helen Chong <contact@helenchong.dev>
2026-01-13 00:36:28 +0800

    chore(bootstrap): specify installing the default dependency group when installing yt-dlp with pipx

--- .config/yadm/bootstrap.d/extra-software.sh
+++ .config/yadm/bootstrap.d/extra-software.sh
@@ -6,7 +6,7 @@ echo "Installing superfile with official install script..."
 bash -c "$(curl -sLo- https://superfile.dev/install.sh)"
 
 echo "Installng yt-dlp with pipx..."
-pipx install yt-dlp
+pipx install yt-dlp[default]
 
 echo "Installing yt-x from repository script..."
 curl -sL "https://raw.githubusercontent.com/Benexl/yt-x/refs/heads/master/yt-x" -o ~/.local/bin/yt-x && chmod +x ~/.local/bin/yt-x
