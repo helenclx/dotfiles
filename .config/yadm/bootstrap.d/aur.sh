1d19ac0b40aa6b06f1994aee47d60e534779c214
Helen Chong <contact@helenchong.dev>
2026-02-04 22:32:04 +0800

    chore(klassy): update configuration for 6.5 and switch to using yay to install the package

--- .config/yadm/bootstrap.d/aur.sh
+++ .config/yadm/bootstrap.d/aur.sh
@@ -16,3 +16,6 @@ install_aur_package "elecwhat-bin" "elecwhat"
 
 echo "Installing topgrade..."
 install_aur_package "topgrade"
+
+echo "installing Klassy, a theming plugin for KDE Plasma..."
+install_aur_package "klassy"
