50f4a489627e78acdd8d156aac59cca52563169c
Helen Chong <contact@helenchong.dev>
2026-02-08 15:21:13 +0800

    feat(Scripts): add private scripts

--- .bashrc.d/path.sh
+++ .bashrc.d/path.sh
@@ -1,5 +1,6 @@
 # Export $PATH
 export PATH="$PATH:$HOME/Scripts"
+export PATH="$PATH:$HOME/Scripts/private"
 export PATH="$PATH:$HOME/bin"
 export PATH="$PATH:$HOME/.local/bin"
 export PATH="$PATH:$HOME/.cargo/bin"
--- .config/fish/conf.d/path.fish
+++ .config/fish/conf.d/path.fish
@@ -1,5 +1,6 @@
 # Export $PATH
 fish_add_path -P $HOME/Scripts
+fish_add_path -P $HOME/Scripts/private
 fish_add_path -P $HOME/bin
 fish_add_path -P $HOME/.local/bin
 fish_add_path -p $HOME/.cargo/bin
