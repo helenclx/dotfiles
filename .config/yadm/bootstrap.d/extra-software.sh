399873c8f6e948a02b26295591b94cdff707cf20
Helen Chong <contact@helenchong.dev>
2026-02-09 21:12:16 +0800

    chore(bootstrap): keep downloaded FreeTube binary's file name instead of renaming it

--- .config/yadm/bootstrap.d/extra-software.sh
+++ .config/yadm/bootstrap.d/extra-software.sh
@@ -14,13 +14,13 @@ echo "Installing yt-x from repository script..."
 curl -sL "https://raw.githubusercontent.com/Benexl/yt-x/refs/heads/master/yt-x" -o ~/.local/bin/yt-x && chmod +x ~/.local/bin/yt-x
 
 echo "Installing the latest version of FreeTube..."
-FREETUBE_LATEST="FreeTube-latest.pacman"
 mkdir -p "$HOME/Downloads" && cd "$HOME/Downloads" || return
-curl -s https://api.github.com/repos/FreeTubeApp/FreeTube/releases |
+FREETUBE_URL=$(curl -s https://api.github.com/repos/FreeTubeApp/FreeTube/releases |
 	grep '"browser_download_url":' |
 	grep '\.pacman"' |
 	head -n 1 |
-	sed -E 's/.*"([^"]+)".*/\1/' |
-	xargs curl -L -o "$FREETUBE_LATEST"
-sudo pacman -U "$FREETUBE_LATEST"
+	sed -E 's/.*"([^"]+)".*/\1/')
+curl -L -O "$FREETUBE_URL"
+FREETUBE_BIN=$(basename "$FREETUBE_URL")
+sudo pacman -U "$FREETUBE_BIN"
 cd "$HOME" || return
