aa15b4669822f7eff41ea9bc95b46de0ee4e8ceb
Helen Chong <contact@helenchong.dev>
2026-05-08 09:53:02 +0800

    chore(bootstrap): reduce code repetition for enabling locales

--- .config/yadm/bootstrap.d/locale.sh
+++ .config/yadm/bootstrap.d/locale.sh
@@ -3,8 +3,11 @@
 echo "Setting up the system locale..."
 
 LOCALE_GEN_FILE=/etc/locale.gen
-# Reference: https://gist.github.com/le0me55i/e1f2244cdc0d64d8e541378357543b15
-sudo sed -e '/en_GB.UTF-8/s/^#*//g' -i $LOCALE_GEN_FILE
-sudo sed -e '/en_SG.UTF-8/s/^#*//g' -i $LOCALE_GEN_FILE
-sudo sed -e '/ms_MY.UTF-8/s/^#*//g' -i $LOCALE_GEN_FILE
+LOCALES=("en_GB.UTF-8" "en_SG.UTF-8" "ms_MY.UTF-8")
+
+for locale in "${LOCALES[@]}"; do
+	# Reference: https://gist.github.com/le0me55i/e1f2244cdc0d64d8e541378357543b15
+	sudo sed -i "/$locale/s/^#*//" "$LOCALE_GEN_FILE"
+done
+
 sudo locale-gen
