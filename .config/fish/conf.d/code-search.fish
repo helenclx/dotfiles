72cb34a1a817469a6a913e3ad4400acca202895a
Helen Chong <contact@helenchong.dev>
2026-06-06 10:47:34 +0800

    fix(fish): configuration for integrating rg, fzf and default editor

--- .config/fish/conf.d/code-search.fish
+++ .config/fish/conf.d/code-search.fish
@@ -2,15 +2,11 @@
 # Reference: https://junegunn.github.io/fzf/tips/ripgrep-integration/
 function rg-fzf
     set RELOAD 'reload:rg --column --color=always --smart-case {q} || :'
-    set OPENER 'if [[ $FZF_SELECT_COUNT -eq 0 ]]; then
-        $EDITOR {1} +{2}         # No selection. Open the current line in the default editor. 
-        else
-            $EDITOR +cw -q {+f}  # Build quickfix list for the selected items.
-        fi'
+    set OPENER 'if [[ $FZF_SELECT_COUNT -eq 0 ]]; then $EDITOR {1} +{2}; else $EDITOR +cw -q {+f}; fi'
     fzf --disabled --ansi --multi \
         --bind "start:$RELOAD" --bind "change:$RELOAD" \
-        --bind "enter:become:$OPENER" \
-        --bind "ctrl-o:execute:$OPENER" \
+        --bind "enter:become:bash -c '$OPENER'" \
+        --bind "ctrl-o:execute:bash -c '$OPENER'" \
         --bind 'alt-a:select-all,alt-d:deselect-all,ctrl-/:toggle-preview' \
         --delimiter : \
         --preview 'bat --style=full --color=always --highlight-line {2} {1}' \
