449c5d345d95e02a2169991bf467e317c96f61c8
Helen Chong <contact@helenchong.dev>
2026-06-13 10:09:05 +0800

    AUR helper: switch to paru from yay, and merge the AUR helper bootstrap script for yadm into the AUR script

--- .config/yadm/bootstrap.d/aur.sh
+++ .config/yadm/bootstrap.d/aur.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 
+AUR_HELPER="paru"
+
+mkdir -p "$HOME/Downloads" && cd "$HOME/Downloads" || return
+
+echo "Compiling $AUR_HELPER, the AUR helper..."
+sudo pacman -S --needed base-devel
+git clone "https://aur.archlinux.org/$AUR_HELPER.git"
+cd $AUR_HELPER || return
+makepkg -si
+
 install_aur_package() {
 	local PACKAGE=$1
 	local LOG_NAME=${2:-$PACKAGE}
-	yay -S "$PACKAGE" 2>&1 | tee "$BOOTSTRAP_LOG_DIR/$LOG_NAME.log"
+	$AUR_HELPER -S "$PACKAGE" 2>&1 | tee "$BOOTSTRAP_LOG_DIR/$LOG_NAME.log"
 }
 
 echo "Installing AUR packages for these config files..."
@@ -16,3 +26,5 @@ install_aur_package "heroic-games-launcher-bin" "heroic-games-launcher"
 
 echo "Installing topgrade..."
 install_aur_package "topgrade"
+
+cd "$HOME" || return
