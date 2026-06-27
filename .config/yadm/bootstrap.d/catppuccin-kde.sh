2bee170415b96ab3bb93fc2c1a5e1faa780702a7
Helen Chong <contact@helenchong.dev>
2026-06-27 09:27:25 +0800

    yadm/bootstrap/catppuccin-kde: configure the script to install the Catppuccin global theme automatically
    Mocha flavour, Mauve accent and the Modern window decorations

--- .config/yadm/bootstrap.d/catppuccin-kde.sh
+++ .config/yadm/bootstrap.d/catppuccin-kde.sh
@@ -2,12 +2,11 @@
 
 echo "Setting up the Catppuccin global theme for KDE Plasma..."
 
-mkdir -p "$HOME/Downloads" && cd "$HOME/Downloads" || exit 1
-
 echo "Cloning the Catppuccin global theme for KDE Plasma into the Downloads folder..."
+mkdir -p "$HOME/Downloads" && cd "$HOME/Downloads" || exit 1
 git clone --depth=1 https://github.com/catppuccin/kde catppuccin-kde && cd catppuccin-kde || exit 1
-echo "Installing the Catppuccin global theme for KDE Plasma..."
-chmod +x install.sh
-./install.sh
+
+echo "Installing the Catppuccin global theme for KDE Plasma: Mocha flavour, Mauve accent with the Modern window decorations..."
+./install.sh 1 4 2 auto
 
 cd "$HOME" || exit 1
